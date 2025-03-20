21af3d1f758d62c7cffd5ee189e439e39701fa42
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2025-03-19 23:37:57 -0500

    Create deploy.php

---
+++ deploy.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "your-mysql-password";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database if it doesn’t exist
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully<br>";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>

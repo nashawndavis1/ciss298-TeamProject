100a58a337039dff828763c6c833d1f5d8b90590
BrendenHoffman <bhessential1@fastmail.com>
2025-03-21 22:04:58 -0500

    schema

---
+++ setup/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS room_type(
+  room_type_id INT NOT NULL AUTO_INCREMENT,
+  room_type_name VARCHAR(32) UNIQUE,
+  total_room INT DEFAULT 3,
+  CONSTRAINT pk_room_type PRIMARY KEY (room_type_id)
+);
+
+CREATE TABLE IF NOT EXISTS reservation(
+  reservation_id INT NOT NULL AUTO_INCREMENT,
+  room_type_id INT NOT NULL,
+  begin_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  confirm_number CHAR(13),
+  CONSTRAINT pk_reservation PRIMARY KEY (reservation_id),
+  CONSTRAINT fk_reservation_room_type FOREIGN KEY (room_type_id) REFERENCES room_type(room_type_id)
+);
